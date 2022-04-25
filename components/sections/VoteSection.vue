2aada2035023624222ce5ee52a7fe79784502bf7
anon <anon@debian-BULLSEYE-live-builder-AMD64>
2022-04-25 13:54:15 +0300

    dev: votes section

--- components/sections/VoteSection.vue
+++ components/sections/VoteSection.vue
@@ -1,25 +1,18 @@
 <template>
-  <div class="text-dark mt-4 mb-4">
+  <div class="text-dark mt-4 mb-4 vote-bg">
     <b-row>
       <b-col cols="9">
-        <div
-          class="p-1"
-          style="
-            background-color: #fff;
-            border-radius: 0 50px 0 0;
-            margin-bottom: -25px;
-          "
-        >
-          <h5>Lorem ipsum dolor</h5>
+        <div class="p-1 vote-header">
+          <h6>Нам интересно ваше мнение</h6>
         </div>
       </b-col>
       <b-col cols="3"></b-col>
     </b-row>
-    <div class="p-2" style="background-color: #fff">
+    <div class="p-2 vote-body">
       <b-row align-v="center" class="h-100">
         <b-col cols="6">
           <b-row>
-            <b-col cols="12">
+            <b-col class="text-center" cols="12">
               <h4>{{ vote }}</h4>
             </b-col>
           </b-row>
@@ -38,25 +31,17 @@
                 ></b-form-radio-group>
               </b-form-group>
             </b-col>
-            <!-- <b-col v-for="answer in answers" :key="answer" cols="12">
-              <b-form-checkbox size="lg">{{ answer.text }}</b-form-checkbox>
-            </b-col> -->
           </b-row>
         </b-col>
       </b-row>
     </div>
     <b-row>
-      <b-col cols="5"></b-col>
-      <b-col class="text-right" cols="7">
-        <div
-          class="p-1"
-          style="
-            background-color: #fff;
-            border-radius: 0 0 0 50px;
-            margin-top: -30px;
-          "
-        >
-          <b-btn variant="warning">Проголосовать</b-btn>
+      <b-col cols="4"></b-col>
+      <b-col class="text-right" cols="8">
+        <div class="p-1 vote-footer">
+          <b-btn style="box-shadow: 0 0 10px #000000" variant="warning"
+            >Проголосовать</b-btn
+          >
         </div>
       </b-col>
     </b-row>
@@ -86,11 +71,29 @@ export default {
 </script>
 
 <style lang="scss">
-.vote-bg {
-  background-color: #fff;
-  transform: skewX(-40deg) skewY(40deg);
-}
 .skew {
   transform: skewX(2deg) skewY(-1deg);
+}
+.vote-bg {
+  border-radius: 5px;
+  background-color: #ffffff50;
+  box-shadow: 0 0 2px rgba(255, 255, 255, 0.5);
+}
+.vote-header {
+  background: linear-gradient(-120deg, transparent 40px, #ffffff 0) top right;
+  background-repeat: no-repeat;
+  border-radius: 5px 0 0 0;
+  // margin-bottom: -2px;
+}
+.vote-body {
+  border-radius: 0 5px 0 5px;
+  background-color: #fff;
+  box-shadow: 0 0 2px rgba(255, 255, 255, 0.5);
+}
+.vote-footer {
+  background: linear-gradient(60deg, transparent 30px, #ffffff 0) bottom right;
+  background-repeat: no-repeat;
+  border-radius: 0 0 5px 0;
+  // margin-top: -2px;
 }
 </style>
