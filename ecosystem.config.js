2a0a71e3171333cdb6dc9f5f4011bbc6c633c9a7
anon <efimovsergeiv@gmail.com>
2022-12-06 13:39:24 +0300

    add:

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'NuxtAppName',
+      name: 'gs-nuxt',
       exec_mode: 'cluster',
       instances: 2, //'max' Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
