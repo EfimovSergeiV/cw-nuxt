8785ec982e8889d1a3e32c23ada04be7918e2dfd
anon <efimovsergeiv@gmail.com>
2022-12-06 16:28:21 +0300

    add:

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'gs-nuxt',
       exec_mode: 'cluster',
-      instances: 2, //'max' Or a number of instances
+      instances: 'max', //'max' Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     }
