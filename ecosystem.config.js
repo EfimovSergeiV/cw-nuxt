c73d3798d6f2a9de642649483764992937de81d1
anon <efimovsergeiv@gmail.com>
2022-12-22 09:04:08 +0300

    to one instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'gs-nuxt',
       exec_mode: 'cluster',
-      instances: 'max', //'max' Or a number of instances
+      instances: 1, //'max' or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     }
